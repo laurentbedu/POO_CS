8ceb790f00274a965202e1e6cf91da54c6cdf2b4
laurentbedu <bedulaurent@gmail.com>
2022-03-16 11:08:02 +0100

    ok

--- POO/CompteBancaire.cs
+++ POO/CompteBancaire.cs
@@ -3,18 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using POO.Sub;
 
 namespace POO
 {
     public class CompteBancaire
     {
-        public string titulaire;
-        public double solde;
-        public string devise;
+        private readonly ClasseDansSub subCls;
+        private string titulaire;
+        private double solde;
+        private string devise;
+        private string interne;
+
+        protected int MyProperty { get; private set; }
+
+        public string Titulaire { get => titulaire; }
+        public string Interne { get => interne; private set => interne = value; }
+        public string Devise { get; set; }
+
+        public string getTitulaire()
+        {
+            return this.titulaire;
+        }
+        public bool setTitulaire(string newValue)
+        {
+            try
+            {
+                this.titulaire = newValue;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public CompteBancaire()
+        {
+            titulaire = "";
+            solde = 0;
+            Devise = "€";
+            subCls = new ClasseDansSub();
+            Interne = "";
+        }
+
+        public CompteBancaire(string _titulaire, double _solde, string _devise)
+        {
+            titulaire = _titulaire;
+            solde = _solde;
+            Devise = _devise;
+        }
+
+        
 
         public void Crediter(double montant)
         {
             solde = solde + montant;
+            Interne = "";
+
+
         }
         public void Debiter(double montant)
         {
--- POO/Form1.cs
+++ POO/Form1.cs
@@ -9,23 +9,41 @@ namespace POO
 
         private void Form1_Shown(object sender, EventArgs e)
         {
+            TestRef();
             CompteBancaire comptePierre;          // déclaration d'un nouvel objet
             comptePierre = new CompteBancaire(); // instanciation de cet objet
             CompteBancaire compteRoger = new CompteBancaire(); //initialisation d'un nouvel objet
 
             // affectations de valeurs aux attributs
-            comptePierre.titulaire = "Pierre";
-            comptePierre.solde = 0;
-            comptePierre.devise = "euros";
+            //comptePierre.titulaire = "Pierre";
+            //comptePierre.solde = 0;
+            //comptePierre.devise = "euros";
 
             // appels des méthodes
             comptePierre.Crediter(300);
             comptePierre.Debiter(500);
+            string titulaire = comptePierre.Titulaire;
+            comptePierre.Interne = "Pierre";
 
-            string description = "Le solde du compte de " + comptePierre.titulaire +
-                " est de " + comptePierre.solde + " " + comptePierre.devise;
+            //string description = "Le solde du compte de " + comptePierre.titulaire +
+            //    " est de " + comptePierre.solde + " " + comptePierre.devise;
             
-            label1.Text = description;
+            //label1.Text = description;
+        }
+
+        private void TestRef()
+        {
+            //CompteBancaire comptePierre = new CompteBancaire();
+            //CompteBancaire compteRobert = comptePierre;
+            //comptePierre.solde = 100;
+            //compteRobert.solde = 200;
+
+            //int a = 10;
+            //int b = a;
+            //a = 20;
+
+            //comptePierre.interne = "";
+
         }
     }
 }
